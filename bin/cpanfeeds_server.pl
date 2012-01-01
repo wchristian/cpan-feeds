9b7f8cd97272e9836eca9649adc68fc8f5ef557f
Christian Walde <walde.christian@googlemail.com>
2012-01-01 04:59:08 +0100

    cleanup

--- bin/cpanfeeds_server.pl
+++ bin/cpanfeeds_server.pl
@@ -4,8 +4,6 @@ use strictures;
 
 package cpanfeeds;
 
-use Test::InDistDir;
 use WWW::CPAN::Feeds;
-use DB::Skip ( pkgs => [qw( Sub::Quote Sub::Defer Method::Generate::Constructor Method::Generate::Accessor warnings strict constant integer Moo::_Utils )] );
 
 WWW::CPAN::Feeds->run_if_script;
